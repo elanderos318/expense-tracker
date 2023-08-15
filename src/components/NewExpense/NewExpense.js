e9451eea43ebb12bbaf29a5c87a59195d8daf8bf
Eduardo Landeros <elanderos318@gmail.com>
2023-08-15 13:22:19 -0500

    import css

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NewExpense.css';
 
 const NewExpense = () => {
     return (
-        <div>
-            <form></form>
+        <div className='new-expense'>
+            
         </div>
     )
 };
